3b856e3cfee387ca4c29ffc4dfb13ee0e00f2e50
dakkang070-sketch <dakkang070@gmail.com>
2026-08-04 15:12:45 +0900

    Add email domain constants for validationadd emailDomains

---
+++ backoffice_admin/constants/emailDomains.ts
@@ -0,0 +1,11 @@
+export const EMAIL_DOMAIN_DIRECT_VALUE = '__custom__';
+
+export const EMAIL_DOMAIN_OPTIONS = [
+  'naver.com',
+  'gmail.com',
+  'daum.net',
+  'kakao.com',
+  'hanmail.net',
+  'outlook.com',
+  'hotmail.com',
+];
